bfccb40b369aa3ac8b0c93199cb923a84e22cb10
olcaytaner <olcaytaner@gmail.com>
2018-11-23 18:16:29 +0300

    Put abstract functions public

--- CrossValidation.h
+++ CrossValidation.h
@@ -11,6 +11,7 @@ using namespace std;
 template <class T> class CrossValidation {
 protected:
     int K;
+public:
     virtual vector<T> getTrainFold(int k) = 0;
     virtual vector<T> getTestFold(int k) = 0;
 };
